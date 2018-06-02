3f5ea8c8a5b3da32c680499e94426a3503ba240d
eynar-pari-gmail <eynar.pari@gmail.com>
2018-06-02 04:00:37 -0400

    sdsaas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -34,18 +34,18 @@ export class LoginComponent implements OnInit {
               private modalService: NgbModal) {
     this.sliders.push(
       {
-        imagePath: 'assets/images/slider1.jpg',
+        imagePath: 'assets/images/colegio1.jpg',
         label: 'First slide label',
         text:
           'Nulla vitae elit libero, a pharetra augue mollis interdum.'
       },
       {
-        imagePath: 'assets/images/slider2.jpg',
+        imagePath: 'assets/images/colegio2.jpg',
         label: 'Second slide label',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
       },
       {
-        imagePath: 'assets/images/slider3.jpg',
+        imagePath: 'assets/images/colegio3.jpg',
         label: 'Third slide label',
         text:
           'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
